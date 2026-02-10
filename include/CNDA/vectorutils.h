388102c7cbb5f4657f8dd47c6e17137d6c7985e5
Parkman145 <ParkerLearmonth@gmail.com>
2026-02-10 11:39:38 -0800

    Refactor project structure

--- include/CNDA/vectorutils.h
+++ include/CNDA/vectorutils.h
@@ -20,4 +20,18 @@ namespace vector_utils {
     bool compatible_shape(std::vector<int> a, std::vector<int> b);
 }
 
+inline bool vector_utils::compatible_shape(std::vector<int> a, std::vector<int> b){
+    if (a.size() != b.size()){
+        return false;
+    }
+
+    for (int i = 0; i < a .size(); i++){
+        if (a[i] == -1 || b[i] == -1) continue;
+
+        if (a[i] != b[i]) return false;
+    }
+
+    return true;
+}
+
 #endif
